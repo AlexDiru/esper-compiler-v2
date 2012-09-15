c9ccd3783e31226168420dbd693b3b292b8d3dc0
Alexander Spedding <alex_slipknot@hotmail.co.uk>
2012-09-16 00:20:41 +0100

    Token constructor added, documentation added

--- src/Token.cs
+++ src/Token.cs
@@ -18,9 +18,35 @@ namespace esper_compiler.src
 
     class Token
     {
+        /// <summary>
+        /// The type of the token
+        /// </summary>
         public TokenType Type;
+
+        /// <summary>
+        /// The value the token holds
+        /// </summary>
         public String Value;
+
+        /// <summary>
+        /// The line that the token first appears on
+        /// </summary>
         public Int32 LineStart;
+
+        /// <summary>
+        /// The column the token first appears on
+        /// </summary>
         public Int32 CharacterPosition;
+
+        /// <summary>
+        /// Set default values for the token
+        /// </summary>
+        public Token()
+        {
+            Type = TokenType.Unknown;
+            Value = String.Empty;
+            LineStart = -1;
+            CharacterPosition = -1;
+        }
     }
 }
