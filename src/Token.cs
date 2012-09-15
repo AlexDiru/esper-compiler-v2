bfae164e64f2d5e299a0891f69f91d9eda40bcd0
Alexander Spedding <alex_slipknot@hotmail.co.uk>
2012-09-16 00:08:21 +0100

    Forget to save file

--- src/Token.cs
+++ src/Token.cs
@@ -5,7 +5,22 @@ using System.Text;
 
 namespace esper_compiler.src
 {
+    enum TokenType
+    {
+        Identifier,
+        Number,
+        String,
+        Character,
+        Symbol,
+        Unknown,
+        EOL
+    }
+
     class Token
     {
+        public TokenType Type;
+        public String Value;
+        public Int32 LineStart;
+        public Int32 CharacterPosition;
     }
 }
